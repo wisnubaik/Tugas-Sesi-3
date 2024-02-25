4c99798aa68437682d3a54b9923a7fc4e51d34d6
wisnubaik <wisnuap2024@gmail.com>
2024-02-25 17:02:11 +0700

    latihan kotlin

---
+++ app/src/main/java/hello.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "Wisnu A. Pradana"
+    val hobby = "baca buku"
+    val hobbyDescription = "meningkatkan knowledge dan mengembangkan good mindset"
+
+    println("Name: $name")
+    println("Hobby: $hobby")
+    println("Description of $hobby: $hobbyDescription")
+}
