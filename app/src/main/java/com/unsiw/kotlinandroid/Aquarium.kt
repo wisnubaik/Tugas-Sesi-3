f89e1ac35f8c9252b5742412a4c60be88f368c36
wisnubaik <wisnuap2024@gmail.com>
2024-03-19 05:18:05 +0700

    codelab-aquarium_1

--- app/src/main/java/com/unsiw/kotlinandroid/Aquarium.kt
+++ app/src/main/java/com/unsiw/kotlinandroid/Aquarium.kt
@@ -1,10 +1,22 @@
 package com.unsiw.kotlinandroid
 
-fun main() {
-    fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
-        return operation(dirty)
-    }
+class Aquarium {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
 
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
+}
+
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
+
+fun main() {
     val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
     println(updateDirty(30, waterFilter))
 
