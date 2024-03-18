565845bd62abbc7e1a7a2564a8aee6edf367c1f4
wisnubaik <wisnuap2024@gmail.com>
2024-03-19 06:53:00 +0700

    aquarium codelab

--- app/src/main/java/com/unsiw/kotlinandroid/Aquarium.kt
+++ app/src/main/java/com/unsiw/kotlinandroid/Aquarium.kt
@@ -1,32 +1,17 @@
-package com.unsiw.kotlinandroid
-interface FishAction {
-    fun eat()
+package com.unsiw.kotlinandroid.decor
+
+data class Decoration2(val rocks: String, val wood: String, val diver: String)
+
+fun makeDecorations() {
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+
+    val (rock, wood, diver) = d5
+    println(rock)
+    println(wood)
+    println(diver)
 }
 
-interface FishColor {
-    val color: String
-}
-
-abstract class AquariumFish : FishColor {
-    abstract override val color: String
-}
-
-class Shark : AquariumFish(), FishAction, FishColor {
-    override val color = "grey"
-    override fun eat() {
-        println("hunt and eat fish")
-    }
-}
-
-class Plecostomus(fishColor: FishColor = GoldColor) : AquariumFish(), FishAction by PrintingFishAction("eat algae"), FishColor by fishColor
-
-object GoldColor : FishColor {
-    override val color = "gold"
-}
-
-class PrintingFishAction(private val food: String) : FishAction {
-    override fun eat() {
-        println("eat algae")
-        println(food)
-    }
+fun main() {
+    makeDecorations()
 }
