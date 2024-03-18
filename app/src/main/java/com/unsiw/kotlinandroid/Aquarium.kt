69b7ad40d95c98c491419d1c3f6ffc46952dfc39
wisnubaik <wisnuap2024@gmail.com>
2024-03-19 06:57:59 +0700

    codelab aquarium

--- app/src/main/java/com/unsiw/kotlinandroid/Aquarium.kt
+++ app/src/main/java/com/unsiw/kotlinandroid/Aquarium.kt
@@ -2,10 +2,14 @@ package com.unsiw.kotlinandroid.decor
 
 data class Decoration2(val rocks: String, val wood: String, val diver: String)
 
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
 fun makeDecorations() {
     val d5 = Decoration2("crystal", "wood", "diver")
     println(d5)
-
+    // Assign all properties to variables.
     val (rock, wood, diver) = d5
     println(rock)
     println(wood)
@@ -14,4 +18,8 @@ fun makeDecorations() {
 
 fun main() {
     makeDecorations()
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
 }
