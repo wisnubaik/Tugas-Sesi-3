f1fbc9dcdb0e3758b41da1df07a4b438a2ac25a0
wisnubaik <wisnuap2024@gmail.com>
2024-03-19 05:21:31 +0700

    codelab-aquarium_2

--- app/src/main/java/com/unsiw/kotlinandroid/main.kt
+++ app/src/main/java/com/unsiw/kotlinandroid/main.kt
@@ -1,16 +1,12 @@
-package id.ac.pnm.ti2b.androidkotlin.com.unsiw.kotlinandroid
+package com.unsiw.kotlinandroid
 
-fun main(args : Array<String>) {
-    biodata("Wisnu", 5)
-    hobby("bikin lagu ama baca buku", "bisa nuangin apa yg dirasain gitu loh,sama memperbaiki pola pikir yang salah serta meningkatkannya")
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
+    myAquarium.height = 60
+    myAquarium.printSize()
 }
-fun biodata(name:String, age:Int) {
-    println("+++++++++++")
-    println("Nama\t: $name")
-    println("Usia\t: $age")
-    println("+++++++++++")
-}
-fun hobby(nama:String, desc:String) {
-    println("saya punya hobby $nama")
-    println("saya ngelakuin sebab itu $desc")
+
+fun main() {
+    buildAquarium()
 }
