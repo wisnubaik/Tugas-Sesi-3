19763554775d3c422eb49be7df52817c3b3557c4
wisnubaik <wisnuap2024@gmail.com>
2024-03-19 09:22:39 +0700

    Codelab Decoration Aquarium

--- app/src/main/java/com/unsiw/kotlinandroid/main.kt
+++ app/src/main/java/com/unsiw/kotlinandroid/main.kt
@@ -1,16 +1,14 @@
 package com.unsiw.kotlinandroid
 
-fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
-    val aquarium6 = Aquarium(numberOfFish = 29)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
 }
 
-fun main() {
-    buildAquarium()
+fun main () {
+    makeFish()
 }
